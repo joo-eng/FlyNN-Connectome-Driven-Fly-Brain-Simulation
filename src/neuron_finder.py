537bc538307e9f20c4cdbcc41f726219903679f5
Daniel Joo <danieljoo887@gmail.com>
2026-09-14 21:10:12 -0400

    Filter neuron data and save to feather format

--- src/neuron_finder.py
+++ src/neuron_finder.py
@@ -4,4 +4,3 @@ neurons = ["LPLC2", "LC4", "LC10", "DNp01", "DNp11", "DNa01", "DNa02", "DNb01", 
 naughty_list = ["LPLC4", "LC40", "LC41", "LC43", "LC44", "LC45", "LC46", "DNp104", "DNp102", "DNp101", "mALC4", "DNp103"]
 df_filtered = df[(df["flywireType"].str.contains("|".join(neurons),case=True, na=False)) & (~df["flywireType"].isin(naughty_list))]
 df_filtered.reset_index(drop=True).to_feather("/Users/daniel/Desktop/Fly/neuron_annotations.feather")
-print(df_filtered["flywireType"].value_counts())
